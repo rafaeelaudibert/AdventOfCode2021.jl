048449082fd9eaea845fe240b1c28f95a6859044
RafaAudibert <rafaeelaudibert@gmail.com>
2021-12-02 00:37:55 +0100

    :fire: Remove dead macro code

--- src/AdventOfCode2021Julia.jl
+++ src/AdventOfCode2021Julia.jl
@@ -19,47 +19,17 @@ function readInput(path::String)
     end
     return s
 end
-function readInput(day::Integer)
-    path = joinpath(@__DIR__, "..", "data", @sprintf("day%02d.txt", day))
-    return readInput(path)
-end
 export readInput
-
-
-# Export a function `dayXY_partZ` for each day and part 1 and 2:
-for d in solvedDays
-    global ds = @sprintf("day%02d.txt", d)
-    global modSymbol = Symbol(@sprintf("Day%02d", d))
-    global dsSymbol_part1 = Symbol("part1")
-    global dsSymbol_part2 = Symbol("part2")
-
-    @eval begin
-        input_path = joinpath(@__DIR__, "..", "data", ds)
-        function $dsSymbol_part1(input::String = readInput($d))
-            return AdventOfCode2021Julia.$modSymbol.$dsSymbol_part1(input)
-        end
-        export $dsSymbol_part1
-
-        function $dsSymbol_part2(input::String = readInput($d))
-            return AdventOfCode2021Julia.$modSymbol.$dsSymbol_part2(input)
-        end
-        export $dsSymbol_part2
-    end
-end
 
 # Benchmark a list of different problems:
 function benchmark(days = solvedDays)
     results = []
     for day in days
         modSymbol = Symbol(@sprintf("Day%02d", day))
-        fSymbol_part1 = Symbol("part1")
-        fSymbol_part2 = Symbol("part2")
         input = readInput(joinpath(@__DIR__, "..", "data", @sprintf("day%02d.txt", day)))
         @eval begin
-            bresult_part1 =
-                @benchmark(AdventOfCode2021Julia.$modSymbol.$fSymbol_part1($input))
-            bresult_part2 =
-                @benchmark(AdventOfCode2021Julia.$modSymbol.$fSymbol_part2($input))
+            bresult_part1 = @benchmark(AdventOfCode2021Julia.$modSymbol.part1($input))
+            bresult_part2 = @benchmark(AdventOfCode2021Julia.$modSymbol.part2($input))
         end
         push!(
             results,
--- src/day01.jl
+++ src/day01.jl
@@ -2,20 +2,19 @@ module Day01
 
 using AdventOfCode2021Julia
 
-function get_reports(input::String)
-    return parse.(Int, split(input))
-end
-
 function part1(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
-    reports = get_reports(input)
+    reports = parse_input(input)
     return count(diff(reports) .> 0)
 end
 
 function part2(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
-    reports = get_reports(input)
-    sums =
-        [a + b + c for (a, b, c) ∈ zip(reports[1:end-2], reports[2:end-1], reports[3:end])]
+    reports = parse_input(input)
+    sums = [a + b + c for (a, b, c) ∈ zip(reports[1:end], reports[2:end], reports[3:end])]
     return count(diff(sums) .> 0)
 end
 
-end # module
+function parse_input(input::String)
+    return parse.(Int, split(input))
+end
+
+end
