f55d784e95f1d2e3c01bc4283931ef9d1c45e5a2
RafaAudibert <rafaeelaudibert@gmail.com>
2021-12-01 17:58:18 +0100

    :bug: Fix incorrect name in day01 methods

--- src/day01.jl
+++ src/day01.jl
@@ -6,16 +6,12 @@ function get_reports(input::String)
     return parse.(Int, split(input))
 end
 
-function day01_part1(
-    input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")),
-)
+function part1(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
     reports = get_reports(input)
     return count(diff(reports) .> 0)
 end
 
-function day01_part2(
-    input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")),
-)
+function part2(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
     reports = get_reports(input)
     sums =
         [a + b + c for (a, b, c) ∈ zip(reports[1:end-2], reports[2:end-1], reports[3:end])]
